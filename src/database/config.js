edde8a477a2e869dc4b5a7737ffcc661ae1e10f7
Andhy Ralac <andhyralac11@gmail.com>
2022-01-16 14:33:11 -0600

    creating file configuration for database

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        username: process.env.USER_NAME_DB,
+        password: process.env.PASSWORD_DB,
+        database: process.env.DATABASE,
+        host: process.env.HOST
+    }
+
+}
